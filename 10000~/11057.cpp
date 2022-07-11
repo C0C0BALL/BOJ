3730318ddb6305b4a8f27b3949bae2de71a6e801
C0C0BALL <99589724+C0C0BALL@users.noreply.github.com>
2022-07-11 16:44:24 +0900

    Create 11057.cpp

---
+++ 10000~/11057.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001][10];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i <= 9; i++) {
+		dp[1][i] = 1;
+	}
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j <= 9; j++) {
+			for (int k = j; k <= 9; k++) {
+				dp[i][j] += dp[i - 1][k] % 10007;
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i <= 9; i++) {
+		ans += dp[n][i];
+	}
+	cout << ans % 10007;
+}
